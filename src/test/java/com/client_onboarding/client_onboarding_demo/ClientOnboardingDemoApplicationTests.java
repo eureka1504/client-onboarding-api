1d736189a01a2d010948eb2be6e21a89a8f99182
eureka1504 <eureka1504@gmail.com>
2026-05-31 23:25:47 -0500

    Disabled default spring boot test, updated build.yml

--- src/test/java/com/client_onboarding/client_onboarding_demo/ClientOnboardingDemoApplicationTests.java
+++ src/test/java/com/client_onboarding/client_onboarding_demo/ClientOnboardingDemoApplicationTests.java
@@ -1,8 +1,10 @@
 package com.client_onboarding.client_onboarding_demo;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class ClientOnboardingDemoApplicationTests {
 
